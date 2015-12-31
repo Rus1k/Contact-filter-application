ee575aa013cdc89d72eab3746d225f6752c6ee98
Rus1k <rfrasulov@gmail.com>
2015-12-31 12:36:58 +0200

    correction Contact

--- src/main/java/com/contactFilter/model/Contact.java
+++ src/main/java/com/contactFilter/model/Contact.java
@@ -1,8 +1,10 @@
 package com.contactFilter.model;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -13,6 +15,8 @@ import javax.persistence.Table;
 @Entity
 @Builder
 @Table(name = "contact")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Contact {
 
     @Id
